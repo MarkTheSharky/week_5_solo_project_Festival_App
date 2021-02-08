0a712fba14dd05092a26447c8e4fc0b9d8460c80
Mark Burns <markburns@gmail.com>
2021-02-08 16:20:13 +0000

    User create new festival complete

--- controllers/festivals_controller.py
+++ controllers/festivals_controller.py
@@ -6,25 +6,32 @@ import repositories.country_repository as country_repository
 
 festivals_blueprint = Blueprint("festival", __name__)
 
-@festivals_blueprint.route("/festivals", methods=['GET'])
+# FESTIVALS HOME
+@festivals_blueprint.route("/festivals")
 def view_all():
     festivals = festival_repository.select_all()
     return render_template("festivals/all_festivals.html", festivals=festivals, title="All Festivals")
+
+# CREATE NEW FESTIVAL
+@festivals_blueprint.route("/festivals/create_festival", methods=['GET'])
+def create_festival():
+    countries = country_repository.select_all()
+    return render_template("festivals/create_festival.html", countries=countries, title="Create New Festival")
+
+# ADD NEW FESTIVAL
+@festivals_blueprint.route("/festivals", methods=['POST'])
+def new_festival():
+    name = request.form['name']
+    country = country_repository.select_by_id(request.form['country_id'])
+    festival = Festival(name, country)
+    festival_repository.save(festival)
+    return redirect("/festivals")
+
+
+
 
 # @festivals_blueprint.route("/festivals/<name>", methods=['GET'])
 # def view_festival(name):
 #     #take name and get id from it
 #     festival = festival_repository.select_by_id()
 #     return render_template("festivals/festival.html", festival=festival)
-
-# CREATE NEW FESTIVAL
-@festivals_blueprint.route("/create_festival", methods=['GET'])
-def create_festival():
-    countries = country_repository.select_all()
-    return render_template("festivals/create_festival.html", countries=countries, title="Create New Festival")
-
-# ADD NEW FESTIVAL
-@festivals_blueprint.route("/festival", methods=['POST'])
-def new_festival():
-
-    return redirect("/festivals")
